c4a53b56b56bd93788737de811e2f99ff1b8da98
winiyaz <winiyaz@proton.me>
2024-07-19 02:43:09 -0700

    - Position

--- 182/main.py
+++ 182/main.py
@@ -1,5 +1,6 @@
 # 182: Understanding the turtle coordinate system
 from turtle import Turtle, Screen
+from rich import print as rprint
 
 to = Turtle()  # Initialize Turtle Class
 to.sc = Screen()  # Initialize Screen Class
@@ -9,6 +10,14 @@ to.sc.bgcolor("#020617")
 to.shapesize(stretch_wid=3, stretch_len=3, outline=1)  # Size
 to.color("#c026d3")  # Set Color of turtle
 to.sc.setup(width=800,height=600)  # Setting the screen size
+
+# -- Actual Work --
+user_bet = to.sc.textinput(title="Makbet", prompt="Colo?")  # Catching an input
+rprint(f'Entered Input {user_bet}')
+
+to.penup()
+to.goto(x=-350, y=0)
+
 
 # --- Exiting on click function
 to.sc.exitonclick()
