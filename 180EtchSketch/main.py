87816c4e6000b1454987d39c899b3c1c223376b4
winiyaz <winiyaz@proton.me>
2024-07-18 23:24:37 -0700

    -Setting up keys - All 4 dira

--- 180EtchSketch/main.py
+++ 180EtchSketch/main.py
@@ -32,10 +32,24 @@ def mo_ba():
 	to.setheading(180)  # This turns the head letting it go backward
 	to.forward(50)
 
+def mo_up():
+	"""Moing Up"""
+	to.setheading(90)
+	to.color("#FFAA80")
+	to.forward(50)
+
+def mo_do():
+	"""Moing Up"""
+	to.setheading(270)
+	to.color("#FF5580")
+	to.forward(50)
+
 
 to.sc.listen()  # Initialize the screen listener function
 to.sc.onkey(mo_fo, "w")
 to.sc.onkey(mo_ba, "e")
+to.sc.onkey(mo_up, "r")
+to.sc.onkey(mo_do, "t")
 
 # --- Exiting on click function
 to.sc.exitonclick()
